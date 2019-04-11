12067f328dac92c4a12ecac11e7d444ee5b01c77
Lctrs <jerome@prmntr.me>
2019-04-11 13:49:19 +0200

    Support configuring underneath panther chrome client

--- src/Extension/Driver/PantherFactory.php
+++ src/Extension/Driver/PantherFactory.php
@@ -8,6 +8,8 @@ use Behat\MinkExtension\ServiceContainer\Driver\DriverFactory;
 use Lctrs\MinkPantherDriver\PantherDriver;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\Definition;
+use function is_array;
+use function is_string;
 
 final class PantherFactory implements DriverFactory
 {
@@ -32,6 +34,47 @@ final class PantherFactory implements DriverFactory
      */
     public function configure(ArrayNodeDefinition $builder) : void
     {
+        $builder
+            ->children()
+                ->arrayNode('chrome')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('binary')->defaultNull()->end()
+                        ->variableNode('arguments')
+                            ->defaultNull()
+                            ->validate()
+                                ->ifTrue(static function ($v) {
+                                    if ($v === null) {
+                                        return false;
+                                    }
+
+                                    if (! is_array($v)) {
+                                        return true;
+                                    }
+
+                                    foreach ($v as $child) {
+                                        if (! is_string($child)) {
+                                            return true;
+                                        }
+                                    }
+
+                                    return false;
+                                })
+                                ->thenInvalid('"arguments" must be an array of strings or null.')
+                            ->end()
+                        ->end()
+                        ->arrayNode('options')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('scheme')->end()
+                                ->scalarNode('host')->end()
+                                ->integerNode('port')->end()
+                                ->scalarNode('path')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
     }
 
     /**
@@ -39,6 +82,12 @@ final class PantherFactory implements DriverFactory
      */
     public function buildDriver(array $config) : Definition
     {
-        return new Definition(PantherDriver::class);
+        return (new Definition(PantherDriver::class))
+            ->setFactory([PantherDriver::class, 'createChromeDriver'])
+            ->setArguments([
+                $config['chrome']['binary'],
+                $config['chrome']['arguments'],
+                $config['chrome']['options'],
+            ]);
     }
 }
